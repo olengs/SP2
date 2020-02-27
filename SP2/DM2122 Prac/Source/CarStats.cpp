cbd4b813ae609b5265fac4f537569aa78decd8e3
ri3n <54059588+ri3n@users.noreply.github.com>
2020-02-28 00:45:27 +0800

    recommiting 28/2-updated shopUI

--- SP2/DM2122 Prac/Source/CarStats.cpp
+++ SP2/DM2122 Prac/Source/CarStats.cpp
@@ -3,7 +3,7 @@ void CarStats::UpgradeOnce()
 {
 	for (int i = 0; i < 3; ++i)
 	{
-		StatLevel[i] += StatLevel[i + 3];
+		++StatLevel[i];
 	}
 }
 
@@ -21,23 +21,29 @@ void CarStats::UpgradeFromStart()
 
 CarStats::CarStats() //set default to 1
 {
-	for (int i = 0; i < 6; ++i)
+	for (int i = 0; i < 5; ++i)
 	{
 		StatLevel[i] = 1.f;
-		StatTRS[i].Scale = Vector3(0.f, 1.f, 1.f);
+		StatTRS[i].Scale = Vector3(0.f, 0.25f, 1.f);
 	}
+	StatUpgrade.Scale = Vector3(0.f, 1.f, 1.f);
 	cost = 500;
 	cost_upgrade = 250;
 	current_upgrade = 0;
 	lock = true;
 }
 
-CarStats::CarStats(float speed, float turbo, float fuel, float speedupgrade, float turboupgrade, float fuelupgrade)
-{
-	StatLevel[0] = speed; StatLevel[1] = turbo; StatLevel[2] = fuel;
-	StatLevel[3] = speedupgrade; StatLevel[4] = turboupgrade; StatLevel[5] = fuelupgrade;
 
-	for (int i = 0; i < 6; ++i) {
+CarStats::CarStats::CarStats(float acceleration, float speed, float turbo, float fuel)
+{
+	//StatLevel[0]: acceleration, StatLevel[1]: speed, StatLevel[2]: turbo, StatLevel[3]: fuel max
+	//StatLevel[4]: fuel current
+	StatLevel[0] = acceleration; StatLevel[1] = speed; StatLevel[2] = turbo;
+	StatLevel[3] = fuel;
+	StatLevel[4] = StatLevel[3];
+	StatUpgrade.Scale = Vector3(0.f, 1.f, 1.f);
+
+	for (int i = 0; i < 5; ++i) {
 		StatTRS[i].Scale = Vector3(0.f, 1.f, 1.f);
 	}
 	cost = 500;
